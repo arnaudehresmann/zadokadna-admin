65aaf4f798dde43e6a1c5008ddaaa2400086019a
Arnaud Ehresmann <arnaud.ehresmann@gmail.com>
2018-11-20 11:28:17 +0100

    integrate firebase

---
+++ src/components/App/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import withAuthentication from './../Session/withAuthentication';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignInPage from '../SignIn';
+import HomePage from '../Home';
+
+import * as ROUTES from '../../constants/routes';
+
+const App = () =>
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr/>
+
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route exact path={ROUTES.HOME} component={HomePage} />
+    </div>
+  </Router>
+
+export default withAuthentication(App);
---
+++ src/components/Session/withAuthentication.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { firebase } from '../../firebase';
+import AuthUserContext from './AuthUserContext';
+
+const withAuthentication = (Component) => {
+  class WithAuthentication extends React.Component {
+    constructor(props) {
+        super(props);
+  
+        this.state = {
+          authUser: null,
+        };
+      }
+  
+      componentDidMount() {
+        this.listener = firebase.auth.onAuthStateChanged(authUser => {
+          authUser
+            ? this.setState({ authUser })
+            : this.setState({ authUser: null });
+        });
+      }
+  
+      componentWillUnmount() {
+        this.listener();
+      }
+
+    render() {
+        const { authUser } = this.state;
+      return (
+        <AuthUserContext.Provider value={authUser}>
+          <Component {...this.props} />
+        </AuthUserContext.Provider>
+      );
+    }
+  }
+
+  return WithAuthentication;
+}
+
+export default withAuthentication;
